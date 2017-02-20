3deafb4301049e46a932964b289659fbb059a7a9
Ashwin Sureshkumar <ashwin.sureshkumar@gmail.com>
2017-02-20 16:46:37 -0500

    feat: Implemented nav profile section

--- src/app/gm-top-nav/gm-top-nav.module.ts
+++ src/app/gm-top-nav/gm-top-nav.module.ts
@@ -4,12 +4,13 @@ import { NavComponent } from './nav/nav.component';
 import { NavLogoComponent } from './nav-logo/nav-logo.component';
 import { NavSearchComponent } from './nav-search/nav-search.component';
 import { NavProfileComponent } from './nav-profile/nav-profile.component';
-import { MdToolbarModule} from '@angular/material';
-
+import { MdToolbarModule } from '@angular/material';
+import { MdIconModule } from '@angular/material';
 @NgModule({
   imports: [
     CommonModule,
-    MdToolbarModule
+    MdToolbarModule,
+    MdIconModule
   ],
   declarations: [NavComponent, NavLogoComponent, NavSearchComponent, NavProfileComponent],
   exports: [NavComponent]
