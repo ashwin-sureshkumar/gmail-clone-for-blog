f33b687f2f61c833a85f9aeddb4c8473cb2601fa
Ashwin Sureshkumar <ashwin.sureshkumar@gmail.com>
2017-02-21 12:32:56 -0500

    chore: Generator to create mock emails using faker

---
+++ generate-mock.ts
@@ -0,0 +1,46 @@
+const faker = require('faker');
+const rimraf = require('rimraf');
+const fs = require('fs');
+
+const sections = [
+  'primary',
+  'promotions',
+  'social',
+  'starred',
+  'important',
+  'sent',
+  'drafts',
+  'trash'
+];
+
+const directory = __dirname + '/src/app/mocks/json';
+
+const createEmail = () => {
+  return {
+    id: faker.random.uuid(),
+    from: {
+      email: faker.internet.email(),
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+    },
+    title: faker.lorem.sentence(),
+    body: faker.lorem.paragraphs()
+  };
+};
+
+const createEmails = (count) => {
+  const emails = [];
+  while (count > 0) {
+    emails.push(createEmail());
+    count--;
+  }
+  return JSON.stringify(emails);
+}
+
+const createSections = (section) => {
+  fs.writeFileSync(`${directory}/${section}.json`, createEmails(10), { flag: 'w' });
+};
+
+
+// delete all mock data
+rimraf(`${directory}/*`, [], () => sections.forEach(createSections));
